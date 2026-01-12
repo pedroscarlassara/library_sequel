593ef7f495f98c5a35f864c0e2884a4822bd0650
Pedro Scarlassara <pedro@scarlassara.com>
2026-01-12 09:46:09 -0300

    feat: print books in book list tab

--- main.py
+++ main.py
@@ -11,20 +11,13 @@ cur = con.cursor()
 
 def list_available_books():
     cur.execute(f'''SELECT * FROM {table_name} WHERE status='available' ''')
-    print(cur.fetchone())
+    book_list_label_1 = customtkinter.CTkLabel(master=tabview.tab("Book List"), text=cur.fetchall())
+    book_list_label_1.pack()
     # talvez seja necessário trocar para fetchall
-
-def borrow_book():
-    print('')
 
 def borrow_book_function():
     print("button pressed")
-    first_name_input = customtkinter.CTkInputDialog(text="First name", title="Book Borrowing")
-    last_name_input = customtkinter.CTkInputDialog(text="Last Name", title="Book Borrowing")
 
-
-
-    first_name = first_name.get_input()
 
 # """ cur.execute(f'''CREATE TABLE {table_name} (
 #     customer_id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -34,7 +27,7 @@ def borrow_book_function():
 #     status TEXT NOT NULL
 #     );''')
 #  """
-# cur.execute(f'''INSERT INTO {table_name} VALUES (2, 'pedro', 'scarlassara', 'livro_2', 'available')''')
+# cur.execute(f'''INSERT INTO {table_name} VALUES (3, 'pedro', 'pereira', 'livro_3', 'available')''')
 # con.commit()
 
 app = customtkinter.CTk()
@@ -51,6 +44,14 @@ home_label_1.pack()
 
 
 tabview.add("Book List")
+
+book_list_button_1 = customtkinter.CTkButton(master=tabview.tab("Book List"), command=list_available_books, text="List Available Books")
+book_list_button_1.pack()
+
+
+
+
+
 tabview.add("Borrow Book")
 tabview.set("Home")
 
