b8fea641e5420a35783d7e31be0ec6599332d159
Pedro Scarlassara <pedro@scarlassara.com>
2026-01-12 09:29:10 -0300

    feat: add tabviews to main window

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import customtkinter
 import sqlite3
+import os
 
 app_version = "0.1"
 
@@ -8,22 +9,54 @@ table_name = "Books"
 con = sqlite3.connect("library.db")
 cur = con.cursor()
 
-""" cur.execute(f'''CREATE TABLE {table_name} (
-    customer_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    first_name TEXT NOT NULL,
-    last_name TEXT,
-    book TEXT NOT NULL,
-    status TEXT NOT NULL
-    );''')
- """
-cur.execute(f'''INSERT INTO {table_name} VALUES (1, 'pedro', 'scarlassara', 'livro_1', 'emprestado')''')
-con.commit()
+def list_available_books():
+    cur.execute(f'''SELECT * FROM {table_name} WHERE status='available' ''')
+    print(cur.fetchone())
+    # talvez seja necessário trocar para fetchall
+
+def borrow_book():
+    print('')
+
+def borrow_book_function():
+    print("button pressed")
+    first_name_input = customtkinter.CTkInputDialog(text="First name", title="Book Borrowing")
+    last_name_input = customtkinter.CTkInputDialog(text="Last Name", title="Book Borrowing")
+
+
+
+    first_name = first_name.get_input()
+
+# """ cur.execute(f'''CREATE TABLE {table_name} (
+#     customer_id INTEGER PRIMARY KEY AUTOINCREMENT,
+#     first_name TEXT NOT NULL,
+#     last_name TEXT,
+#     book TEXT NOT NULL,
+#     status TEXT NOT NULL
+#     );''')
+#  """
+# cur.execute(f'''INSERT INTO {table_name} VALUES (2, 'pedro', 'scarlassara', 'livro_2', 'available')''')
+# con.commit()
 
 app = customtkinter.CTk()
-app.geometry("400x400")
-app.title(f"library sequel {app_version}")
+app.geometry("400x300")
+app.title(f"Library Sequel")
 
-label_query_output = customtkinter.CTkLabel(app, text="Query Output Label", fg_color="transparent")
-label_query_output.pack()
+tabview = customtkinter.CTkTabview(master=app)
+tabview.pack(padx=20, pady=20)
+
+tabview.add("Home")
+
+home_label_1 = customtkinter.CTkLabel(master=tabview.tab("Home"), text=f"Welcome, {os.getlogin()}.")
+home_label_1.pack()
+
+
+tabview.add("Book List")
+tabview.add("Borrow Book")
+tabview.set("Home")
 
 app.mainloop()
+
+# CRIAR  FUNÇÃO DE EMPRESTAR LIVRO
+
+# CRIAR FUNÇÃO DE DEVOLVER
+
