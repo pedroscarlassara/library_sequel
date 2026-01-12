cd742b77a3671099c0b5dc9a31ecb7a6eef53c8b
Pedro Scarlassara <pedro@scarlassara.com>
2026-01-12 08:40:25 -0300

    feat: create sqlite table

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 import customtkinter
+import sqlite3
 
 app_version = "0.1"
+
+table_name = "Books"
+
+con = sqlite3.connect("library.db")
+cur = con.cursor()
+
+""" cur.execute(f'''CREATE TABLE {table_name} (
+    customer_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT NOT NULL,
+    last_name TEXT,
+    book TEXT NOT NULL,
+    status TEXT NOT NULL
+    );''')
+ """
+cur.execute(f'''INSERT INTO {table_name} VALUES (1, 'pedro', 'scarlassara', 'livro_1', 'emprestado')''')
+con.commit()
 
 app = customtkinter.CTk()
 app.geometry("400x400")
